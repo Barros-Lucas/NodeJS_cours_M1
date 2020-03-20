7be5e4e8300cf0fb8b05dff392f9aac69f59aed8
lucas <lucas.barros@hotmail.fr>
2020-03-20 16:34:14 +0100

    Tout le server dans le callback de la lecture du fichier, et utilisation de data pour remplir notre parametre pour pug

--- Server.js
+++ Server.js
@@ -1,36 +1,57 @@
-const http = require("http");
-const pug = require("pug");
+let fs = require('fs');
+let fileName = process.argv[2];
+if(fileName!=undefined){
+    fs.readFile(fileName,'utf8',(err,data)=>{
+        if(err){
+            console.error(err)
+            return
+        }
+            const http = require("http");
+            const pug = require("pug");
 
-const compiledFunction = pug.compileFile('template.pug');
-const port = 3000;
+            const compiledFunction = pug.compileFile('template.pug');
+            const port = 3000;
 
+            var resultSplit = data.split("\n");
+            const dataUserVille = {};
+            var users = [];
+            resultSplit.forEach(function (elt){
+                var UserVilleSplit = {};
+                var resultSplitUsers = elt.split(";")
+                UserVilleSplit.id = resultSplitUsers[0];
+                UserVilleSplit.ville = resultSplitUsers[1];
+                users.push(UserVilleSplit);
+            })
+            dataUserVille.users = users;
+            // const dataUserVille = {
+            //     "users":[
+            //         {
+            //             "id":"User1",
+            //             "ville":"toulouse"
+            //         },
+            //         {
+            //             "id":"User2",
+            //             "ville":"toulouse"
+            //         },
+            //     ]
 
-const dataUserVille = {
-    "users":[
-        {
-            "id":"User1",
-            "ville":"toulouse"
-        },
-        {
-            "id":"User2",
-            "ville":"toulouse"
-        },
-    ]
+            // }
 
+            const server = http.createServer((req, res) =>{
+
+                const generatedTemplate = compiledFunction({
+                    dataP: dataUserVille
+                });
+
+                res.statusCode = 200;
+                res.setHeader("Content-Type", "text/html");
+                res.end(generatedTemplate);
+            });
+
+            server.listen(port, () =>{
+                console.log(`Server running at port ${port}`);
+            })
+    })
+}else{
+    console.log("Missing argument| Exemple : node ./script.js data.csv")
 }
-
-const server = http.createServer((req, res) =>{
-
-    const generatedTemplate = compiledFunction({
-        name: 'Lucas',
-        dataP: dataUserVille
-    });
-
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end(generatedTemplate);
-});
-
-server.listen(port, () =>{
-    console.log(`Server running at port ${port}`);
-})
